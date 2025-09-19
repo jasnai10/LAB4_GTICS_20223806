3b534487149c9c2fab224de2f6f9f2102fbb2fc7
Jair <a20223806@pucp.edu.pe>
2025-09-18 20:16:44 -0500

    feat: Archivos iniciales

---
+++ src/main/java/com/example/lab4_20223806/Lab420223806Application.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20223806;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420223806Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab420223806Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/lab4_20223806/Lab420223806ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab4_20223806;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420223806ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
